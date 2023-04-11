3288ed92df9a5e12c6962055b590452bec5c6dcd
Tommy-85 <93783577+Tommy-85@users.noreply.github.com>
2023-04-11 11:05:43 +0000

    Aufgabe 3 check

--- 01_restaurant/challenges/01_03_menus_ch.sql
+++ 01_restaurant/challenges/01_03_menus_ch.sql
@@ -1,6 +1,9 @@
 -- Kreieren Sie eine Liste, die verwendet wird um drei Menues zu erstellen.
 
 -- Kreieren Sie eine Liste aller Speisen. Sortieren Sie diese nach dem Preis beginnend beim niedrigsten.
+SELECT Name, Beschreibung, Preis, Typ FROM Speise ORDER BY Preis;
 -- Kreieren Sie eine Liste von Vorspeisen und Getraenken.
+SELECT Name, Beschreibung, Preis, Typ FROM Speise WHERE Typ = 'Vorspeise' OR Typ = 'Getränk' ORDER BY Typ;
 -- Kreieren Sie eine Liste des ganzen Angebots ausgenommen der Getränke.
+SELECT Name, Beschreibung, Preis, Typ FROM Speise WHERE Typ != 'Getränk' ORDER BY Typ;
 -- Die letzten beiden Menues sollen nach Gericht-Art sortiert sein.
