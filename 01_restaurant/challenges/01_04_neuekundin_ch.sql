1e1ed2945378ded9d59408a9fa7f22a27e0340ed
Tommy-85 <93783577+Tommy-85@users.noreply.github.com>
2023-05-23 12:59:12 +0000

    Aufgabe 4 check

--- 01_restaurant/challenges/01_04_neuekundin_ch.sql
+++ 01_restaurant/challenges/01_04_neuekundin_ch.sql
@@ -5,3 +5,7 @@
 -- 479 Lapis Dr., Memphis, TN
 -- Telefon: (555) 555-1212; Geburtstag: Juli 21, 1973
 
+INSERT INTO Kunden
+(Vorname, Nachname, Email, Adresse, Stadt, State, Telefon, Geburtstag)
+VALUES ("Anna", "Smith", "asmith@samoca.org", "479 LapisDr.", "Memphis", "TN", "555-555-1212", "1973-07-21")
+SELECT * FROM Kunden ORDER BY KundenID DESC
