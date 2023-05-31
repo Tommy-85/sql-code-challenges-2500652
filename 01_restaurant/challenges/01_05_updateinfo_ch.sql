9e96bc9cbf0158955769b0ce59c0ce0c34be3ede
Tommy-85 <93783577+Tommy-85@users.noreply.github.com>
2023-05-31 12:38:22 +0000

    Aufgabe 5 Check

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,5 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+SELECT * FROm Kunden WHERE Vorname = 'Taylor' AND Nachname = 'Jenkins';
+UPDATE Kunden SET Adresse = '74 Pine St.', Stadt = 'New York', State = 'NY' WHERE KundenID = 26;
